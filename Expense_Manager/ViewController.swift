f21cd88beae5ab1975fd491f397b4e6051d66587
Angel-Mor <angel4761.be23@chitkara.edu.in>
2025-07-22 14:26:24 +0530

    Expense Manager
    I have uploaded my work.

---
+++ Expense_Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense_Manager
+//
+//  Created by Angel Mor on 19/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Expense_ManagerTests/Expense_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_ManagerTests.swift
+//  Expense_ManagerTests
+//
+//  Created by Angel Mor on 19/07/25.
+//
+
+import Testing
+@testable import Expense_Manager
+
+struct Expense_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
